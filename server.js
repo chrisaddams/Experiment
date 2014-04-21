a43f784902888fcf93b09bac60af66a0b0e0aacb
Chris_Gornall <chris.gornall@gmail.com>
2014-04-21 22:26:12 +0100

    Now with UI

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var app = express();
 function compile(str, path){
 	return stylus(str).set('filename', path);
-}
+};
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 app.use(require("express-chrome-logger"));
